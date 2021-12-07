08804b3fe714e337c94e766b6cc206762e54ced4
Rankaisija <65511061+turpaan64@users.noreply.github.com>
2021-12-07 23:34:47 +0200

    Update z64.c

--- src/z64.c
+++ src/z64.c
@@ -45,6 +45,8 @@ void z64_Init(
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// TODO Set this in settings?
+	// glfwWindowHint(GLFW_SAMPLES, 4);
 	
 	#ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
