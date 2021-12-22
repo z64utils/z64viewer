df93c890299922d962211061da3cc915c43d69a7
Rankaisija <65511061+turpaan64@users.noreply.github.com>
2021-12-22 23:39:29 +0200

    Light - Fog Done :confetti_ball:

--- src/Light.c
+++ src/Light.c
@@ -8,8 +8,8 @@ void Light_BindLights(Scene* scene) {
 	
 	OsAssert(lightCtx->envLight != NULL);
 	EnvLight* envLight = &lightCtx->envLight[Wrap(lightCtx->curLightId, 0, lightCtx->lightListNum)];
-	f32 fogParam[2];
-	f32 fogColor[3];
+	f32 uFog[2];
+	f32 uFogColor[3];
 	f32 light[16] = {
 		/* 0x0 */ HU8(envLight->ambient.r), HU8(envLight->ambient.g), HU8(envLight->ambient.b),
 		/* 0x3 */ HU8(envLight->colorA.r), HU8(envLight->colorA.g), HU8(envLight->colorA.b),
@@ -18,34 +18,38 @@ void Light_BindLights(Scene* scene) {
 		/* 0xC */ (f32)envLight->dirB.x / 127.0, (f32)envLight->dirB.y / 127.0, (f32)envLight->dirB.z / 127.0,
 	};
 	
-	f32 near = (ReadBE(envLight->fogNear) & 0x3FF) * 100;
-	f32 far = ReadBE(envLight->fogFar) * 100;
-	f32 fogM = (50000.0 * 25600.0) / (far - near);
-	f32 fogO = (50000.0 - near) * 25600.0 / (far - near);
+	f32 near = (ReadBE(envLight->fogNear) & 0x3FF);
+	f32 far = 1000; // Hardcoded to this value in OoT. z_play.c function Gameplay_SetFog
+	f32 fogMultiply;
+	f32 fogOffset;
 	
-	fogParam[0] = fogM;
-	fogParam[1] = fogO;
+	if (near >= 1000) {
+		fogMultiply = 0;
+		fogOffset = 0;
+	} else if (near >= 997) {
+		fogMultiply = 3276700.0;
+		fogOffset = 3302400.0;
+	} else if (near < 0) {
+		fogMultiply = 0;
+		fogOffset = 25500.0;
+	} else {
+		fogMultiply = (500.0 * 256.0) / (far - near);
+		fogOffset = (500.0 - near) * 256.0 / (far - near);
+	}
+	
+	uFog[0] = fogMultiply;
+	uFog[1] = fogOffset;
 	
 	if (lightCtx->state & LIGHT_STATE_CHANGED) {
 		lightCtx->state &= ~LIGHT_STATE_CHANGED;
-		near *= 0.01;
-		far *= 0.01;
 		
 		OsPrintfEx("LightID:    %6X", lightCtx->curLightId);
-		OsPrintf("Near:       %6.0f", near);
-		OsPrintf("Far:        %6.0f", far);
-		OsPrintf("Multiplier: %6.0f", fogM);
-		OsPrintf("Offset:     %6.0f", fogO);
-		f32 fogM = (500.0 * 256.0) / (far - near);
-		f32 fogO = (500.0 - near) * 256.0 / (far - near);
-		OsPrintf("VanMult:    %6.0f", fogM);
-		OsPrintf("VanOffst:   %6.0f", fogO);
 	}
 	
 	for (s32 i = 0; i < 3; i++) {
-		fogColor[i] = (f32)envLight->fogColor.c[i] / 255.0;
+		uFogColor[i] = (f32)envLight->fogColor.c[i] / 255.0;
 	}
 	
-	n64_set_fog(fogParam, fogColor);
+	n64_set_fog(uFog, uFogColor);
 	n64_set_lights(light);
 }
